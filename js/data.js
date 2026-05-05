8e933d7650b7fd6f7b4fd8a0431999a3e17e7f10
vesselvi <ignacio.caram.43@gmail.com>
2026-05-05 11:28:35 -0300

    cree los archivos y layout del projecto

---
+++ js/data.js
@@ -0,0 +1,34 @@
+export const coche = [
+    {
+        marca: "Toyota",
+        modelo: "Corolla",
+        año: 2022,
+        color: "Blanco",
+        estado: "Nuevo",
+        precio: 18500
+    },
+    {
+        marca: "Ford",
+        modelo: "Focus",
+        año: 2018,
+        color: "Gris",
+        estado: "Usado",
+        precio: 9500
+    },
+    {
+        marca: "Chevrolet",
+        modelo: "Cruze",
+        año: 2020,
+        color: "Negro",
+        estado: "Usado",
+        precio: 14000
+    },
+    {
+        marca: "Volkswagen",
+        modelo: "Golf",
+        año: 2019,
+        color: "Rojo",
+        estado: "Usado",
+        precio: 16000
+    }
+];
